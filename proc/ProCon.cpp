978c7f42f2562830f08f8aa3dd68e74c37df3107
Thuan Tang <thuantang@gmail.com>
2019-10-16 20:46:43 -0700

    Added producer/consumer program

--- proc/ProCon.cpp
+++ proc/ProCon.cpp
@@ -1,7 +1,120 @@
+#include <pthread.h>
+#include <semaphore.h>
 #include <iostream>
 
+// GLOBAL
+const int BUF_SZ = 5;
+int IN = 0, OUT = 0;
+bool STOP = false;
+sem_t PRODUCE, CONSUME, CONTROLLER;
+
+void *consumer(void *args) {
+    char chr, *buf = (char *)args;
+
+    while(true) {
+        sem_wait(&CONSUME);
+
+        if(STOP) break;
+
+        chr = buf[OUT];
+        std::cout << chr << std::endl;
+
+        ++OUT %= BUF_SZ;
+        sem_post(&CONTROLLER);
+
+        sem_post(&PRODUCE);
+    }
+    pthread_exit(0);
+}
+
+void *producer(void *args) {
+    char chr = 33, *buf = (char *)args;
+
+    while(true) {
+        sem_wait(&PRODUCE);
+
+        if(STOP) break;
+        if(chr > 126) chr = 33;  // reset ascii character
+
+        buf[IN] = chr++;
+        // std::cout << "proudcing " << buf[IN] << std::endl;
+        ++IN %= BUF_SZ;
+
+        sem_post(&CONSUME);
+    }
+    pthread_exit(0);
+}
+
+// The controller will stop consumer/producer
+void *controller(void *args) {
+    int limit = *(int *)args;
+
+    for(int i = 0; i < limit; ++i) sem_wait(&CONTROLLER);
+
+    // signal consumer/producer with flag STOP = true
+    STOP = true;
+    sem_post(&CONSUME);
+    sem_post(&PRODUCE);
+
+    pthread_exit(0);
+}
+
 int main() {
-    // TODO
+    int count_limit = 10;
+    char buf[BUF_SZ];
+    pthread_t tid[3];
+
+    std::cout << "Simulating buffer empty" << std::endl;
+
+    sem_init(&CONTROLLER, 0, 0);
+    sem_init(&CONSUME, 0, 0);       //  init empty buffer
+    sem_init(&PRODUCE, 0, BUF_SZ);  // producer need to produce by BUF_SZ times
+
+    pthread_create(&tid[0], NULL, controller, &count_limit);
+    pthread_create(&tid[1], NULL, consumer, buf);
+    pthread_create(&tid[2], NULL, producer, buf);
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_join(tid[2], NULL);
+
+    std::cout << "\n\nSimulating buffer full" << std::endl;
+
+    // init full buffer data
+    for(int i = 0; i < BUF_SZ; ++i) buf[i] = i + 'A';
+
+    sem_init(&CONTROLLER, 0, 0);
+    sem_init(&CONSUME, 0, BUF_SZ);  // init full buffer of BUF_SZ
+    sem_init(&PRODUCE, 0, 0);       // producer can't produce
+    STOP = false;
+    OUT = 0;
+    IN = 0;
+
+    pthread_create(&tid[0], NULL, controller, &count_limit);
+    pthread_create(&tid[1], NULL, consumer, buf);
+    pthread_create(&tid[2], NULL, producer, buf);
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_join(tid[2], NULL);
+
+    std::cout << "\n\nSimulating buffer not empty" << std::endl;
+
+    // init full buffer data
+    buf[0] = 'a';
+    buf[1] = 'b';
+
+    sem_init(&CONTROLLER, 0, 0);
+    sem_init(&CONSUME, 0, 2);  // init buffer has 2 characters
+    sem_init(&PRODUCE, 0, 3);  // producer need to produce 3 characters
+    STOP = false;
+    OUT = 0;
+    IN = 2;
+
+    pthread_create(&tid[0], NULL, controller, &count_limit);
+    pthread_create(&tid[1], NULL, consumer, buf);
+    pthread_create(&tid[2], NULL, producer, buf);
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_join(tid[2], NULL);
 
     return 0;
 }
